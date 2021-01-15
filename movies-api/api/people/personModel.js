032f9645d61b50aed8621e83d739faac8ad1b6a1
Mateusz Kolodziejczyk <mateuszlk1998@gmail.com>
2021-01-15 00:30:15 +0000

    Created PersonSchema

--- movies-api/api/people/personModel.js
+++ movies-api/api/people/personModel.js
@@ -5,31 +5,20 @@ const Schema = mongoose.Schema;
 
 const PersonSchema = new Schema({
 
-  adult: { type: Boolean },
+  birthday: { type: String},
+  known_for_department: { type: String },
+  deathday: { type: String },
   id: { type: Number, required: true, unique: true },
-  poster_path: { type: String },
-  overview: { type: String },
-  release_date: { type: String },
-  original_title: { type: String },
-  genre_ids: [{ type: Number }],
-  original_language: { type: String },
-  title: { type: String },
-  backdrop_path: { type: String },
+  name: { type: String },
+  also_known_as: [{ type: String}],
+  gender: { type: Number },
+  biography: { type: String },
   popularity: { type: Number },
-  vote_count: { type: Number },
-  video: { type: Boolean },
-  vote_average: { type: Number },
-  production_countries: [{
-    iso_3166_1: { type: String },
-    name: { type: String }
-  }],
-  runtime: { type: Number },
-  spoken_languages: [{
-    iso_639_1: { type: String },
-    name: { type: String }
-  }],
-  status: { type: String },
-  tagline: { type: String }
+  place_of_bith: { type: String },
+  profile_path: { type: Number },
+  adult: { type: Boolean },
+  imdb_id: { type: String },
+  homepage: { type: String},
 });
 
 PersonSchema.statics.findByMovieDBId = function (id) {
